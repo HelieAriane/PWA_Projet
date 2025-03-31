d7a0cfe418ba8ee4f36411b7b0f616cc6476b2fb
Ariane Helie <arianehelie@gmail.com>
2025-03-31 19:14:51 -0400

    alert

--- avis-alertes/src/components/SubscribeButton.jsx
+++ avis-alertes/src/components/SubscribeButton.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
 
 function SubscribeButton() {
-  const [message, setMessage] = useState("");
-
   const handleSubscribe = () => {
-    setMessage("La fonctionnalité n'est pas encore disponible.")
+    alert("La fonctionnalité n'est pas encore disponible.")
   };
 
   return (
     <div>
       <button className="subscribeButton" onClick={handleSubscribe}>M'abonner</button>
-      {message && <p>{message}</p>}
     </div>
   )
 }
