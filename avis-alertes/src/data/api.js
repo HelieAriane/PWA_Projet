d2cffe81394e400410cdba2d702bbee863e1a0c8
Ariane Helie <arianehelie@gmail.com>
2025-04-01 15:36:46 -0400

    api + cache

---
+++ avis-alertes/src/data/api.js
@@ -0,0 +1,24 @@
+import { saveInCache } from "../utils/cache";
+
+const API_URL = "https://donnees.montreal.ca/api/3/action/datastore_search"
+const RESOURCE_ID = "fc6e5f85-7eba-451c-8243-bdf35c2ab336"
+
+async function fetchAlerts(limit = 20) {
+  const url = `${API_URL}?resource_id=${RESOURCE_ID}&limit=${limit}`;
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+
+      saveInCache('data', json.records)
+
+      return json.result;
+  } catch (error) {
+      console.error(error);
+  }
+}
+
+export default fetchAlerts
