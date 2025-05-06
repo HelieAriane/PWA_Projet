bc3f58ccfd4aad607ae2f2e1ee0b309af3bc2f83
Ariane Helie <arianehelie@gmail.com>
2025-05-05 23:56:14 -0400

    function registerSW()

--- avis-alertes/src/main.jsx
+++ avis-alertes/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 function registerSW() {
-  if ('serviceWorker' in navigator && import.meta.en.PROD) {
+  if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/sw.js')
         .then((registration) => {
