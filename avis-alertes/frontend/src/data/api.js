92d354fb914f73b2adf09aac1713d8362d1bb26e
Ariane Helie <arianehelie@gmail.com>
2025-05-25 20:54:58 -0400

    correction URL

--- avis-alertes/frontend/src/data/api.js
+++ avis-alertes/frontend/src/data/api.js
@@ -4,14 +4,14 @@ const API_URL_JSON = "https://donnees.montreal.ca/api/3/action/datastore_search"
 const RESOURCE_ID = "fc6e5f85-7eba-451c-8243-bdf35c2ab336"
 
 // On lit l'URL du backend à partir d'une variable d’environnement Vite
-const API_URL_BACKEND = import.meta.env.VITE_BACKEND_URL + "/api/alerts";
+const API_URL_BACKEND = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 let DATA = [];
 
 async function fetchAlerts() {
   //const url = `${API_URL_JSON}?resource_id=${RESOURCE_ID}&limit=${limit}`;
   try {
-    const response = await fetch(API_URL_BACKEND);
+    const response = await fetch(`${API_URL_BACKEND}/api/alerts`);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
