2f592ce841f65ca6e76f6de2bc8deaae0b77b487
Ariane Helie <arianehelie@gmail.com>
2025-05-26 15:55:03 -0400

    npm run build

--- avis-alertes/frontend/public/sw.js
+++ avis-alertes/frontend/public/sw.js
@@ -8,7 +8,7 @@ const BACKEND_URL = "https://avis-alertes-arianehelie.onrender.com";
 const urlsToCache = [
   '/',
   '/index.html',
-  '/assets/index-ct8bexoe.js',
+  '/assets/index-DLu9koW2.js',
   '/assets/index-DFxTzgur.css',
   '/assets/logo_footer-BnavPvK6.svg',
   '/assets/logo_header-4dMxm99W.svg',
